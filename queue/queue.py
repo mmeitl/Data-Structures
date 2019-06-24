7142ed6ab256cbbb9750ed28fde848897b3505b2
Matthew Meitl <DarkSamus6@gmail.com>
2019-06-24 15:14:47 +0000

    finished queue

--- queue/queue.py
+++ queue/queue.py
@@ -26,9 +26,22 @@ class Queue:
   def len(self):
     return self.size
 
-  def len(self):
-    pass
+class Node:
+	def __init__(self, value=None, next_node=None):
+		self.value = value
+		self.next_node = next_node
 
+ 	def get_value(self):
+		return self.value
+
+ 	def get_next(self):
+		return self.next_node
+
+ 	def set_next(self, new_next):
+		self.next_node = new_next
+
+ 	def set_value(self, new_value):
+		self.value = new_value
 
 class Linked_List:
 	def __init__(self):
